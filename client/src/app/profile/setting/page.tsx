f600e541b61a6f49cd1c0278aab81badea748f0f
MAOZ <maooz4426@gmail.com>
2024-10-26 18:41:03 +0900

    プロフィール編集画面作成

---
+++ client/src/app/profile/setting/page.tsx
@@ -0,0 +1,7 @@
+import { ProfileSetting } from "@/features/profile/components/ProfileSetting";
+
+const ProfileSettingPage = () => {
+	return <ProfileSetting />;
+};
+
+export default ProfileSettingPage;
