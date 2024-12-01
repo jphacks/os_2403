824fa821bb12356892db54319dc05842ee405480
MAOZ <maooz4426@gmail.com>
2024-12-02 00:18:06 +0900

    fix

--- client/src/app/user/signup/user/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-import { SignUpDialog } from "@/features/signup/components/signup";
-
-const SignupPage = () => {
-  return <SignUpDialog type={"user"} />;
-};
-
-export default SignupPage;
