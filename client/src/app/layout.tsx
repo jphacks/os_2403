b09fb51ad3adb9ea6b9f27416f015f124ef14305
MAOZ <maooz4426@gmail.com>
2024-12-06 19:28:49 +0900

    fix

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "@/styles/globals.scss";
 import { Toaster } from "@/components/ui/sonner";
 import { BackGroundColor } from "@/features/account/components/Background";
+import { Menubar } from "@/features/menubar";
 
 export const metadata: Metadata = {
   title: "HubMe",
